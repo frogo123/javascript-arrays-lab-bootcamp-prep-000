d5704bc84367dd983f81b795f42cf044c46f75a6
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-10 09:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ kittens.push(name);
 return kittens;
 }
 
-kittens = ["Milo", "Otis", "Garfield"];
-function destructivelyPrependKitten(name){
-  kittens.pop(name);
-  return kittens;
-}
+// kittens = ["Milo", "Otis", "Garfield"];
+// function destructivelyPrependKitten(name){
+//   kittens.pop(name);
+//   return kittens;
+// }
